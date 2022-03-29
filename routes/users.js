7e02dfd8a742fb0d0c9e2011ca3759b097300613
Northern-W <1547339416@qq.com>
2022-03-29 16:25:34 +0800

    移除wx部分

--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,6 @@ const router = express.Router();
  * 注册管理员
  * 注册咨询师
  * 注册督导
- * 注册访客
  */
 
 // @route   POST /users/admin
@@ -306,95 +305,6 @@ router.post(
           // Add student details in the DB
           await promisePool.query(
               `INSERT INTO supervisor (sup_id, sup_name, sup_gender, sup_phone, sup_status) VALUES (${user_id},"${sup_name}", "${sup_gender}", "${sup_phone}", "offline")`
-          );
-
-          // Create a token
-          const token = jwt.sign(payload, config.get("jwtSecret"), { expiresIn: 21600, });
-
-          // Create an httpOnly cookie
-          res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV !== "development", maxAge: 6 * 60 * 60 * 1000 });
-
-          // Send success message to the client
-          res.send("Logged in");
-        }
-      } catch (err) {
-        // Catch errors
-        throw err;
-      }
-    }
-);
-
-
-// @route   POST /users/wx/visitor
-// @desc    注册访客
-// @access  Public
-router.post(
-    "/wx/visitor", [
-      check("user_name", "username is required").notEmpty(), // Check the username
-      check(
-          "user_password",
-          "Please enter a password with 6 or more characters"
-      ).isLength({ min: 6 }), // Check the password
-      check("visitor_phone", "Phone is required").notEmpty(), // Check the phone
-    ],
-    async(req, res) => {
-      // Check for errors
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        // Return the errors
-        return res.status(400).json({ errors: errors.array() });
-      }
-
-      // Extract info from the body
-      let {
-        user_name,
-        user_password,
-        visitor_phone,
-      } = req.body;
-
-      try {
-        // Check if user exists
-        const [rows] = await promisePool.query(
-            `SELECT EXISTS(SELECT * from login WHERE user_name = "${user_name}" ) "EXISTS" FROM dual`
-        );
-        const result = rows[0].EXISTS;
-
-        if (result) {
-          // User already exists
-          return res.status(200).json(
-            {
-            "data":{
-              "errCode":2,
-              "errMessage":"用户名已存在！"
-              }
-            });
-        } else {
-          // Encrypt Password
-          const salt = await bcrypt.genSalt(10);
-          user_password = await bcrypt.hash(user_password, salt);
-
-          // Add user details in the DB
-          await promisePool.query(
-              `INSERT INTO login (user_name, user_password, role) VALUES ("${user_name}", "${user_password}", "visitor")`
-          );
-
-          // Create payload for token
-          const payload = {
-            id: 0,
-          };
-
-          // Get user id
-          const [rows] = await promisePool.query(
-              `SELECT user_id from login WHERE user_name='${user_name}'`
-          );
-
-          // Store user id in payload for token
-          const user_id = rows[0].user_id;
-          payload.id = user_id;
-
-          // Add student details in the DB
-          await promisePool.query(
-              `INSERT INTO visitor (visitor_id, visitor_name, visitor_gender, visitor_phone, visitor_status) VALUES (${user_id},"${visitor_name}", "${visitor_gender}", "${visitor_phone}", "offline")`
           );
 
           // Create a token
