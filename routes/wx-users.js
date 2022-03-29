f23aff84f8aa75d93807595415aca1a59d73f78d
Northern-W <1547339416@qq.com>
2022-03-29 19:45:07 +0800

    添加editInfo

--- routes/wx-users.js
+++ routes/wx-users.js
@@ -3,6 +3,7 @@ const express = require("express"); // Create router
 const bcrypt = require("bcryptjs"); // Encrypt password
 const jwt = require("jsonwebtoken"); // Authorization
 const config = require("config"); // Global variables
+const auth = require("../middleware/auth"); // Middleware
 const { check, validationResult } = require("express-validator"); // Check and validate the inputs
 const promisePool = require("../config/db"); // Import instance of mysql pool
 
@@ -109,6 +110,109 @@ router.post(
         // Catch errors
         throw err;
       }
+    }
+);
+
+// @route   PUT /wx-users/editInfo
+// @desc    修改访客信息
+// @access  Private
+router.put(
+    "/editInfo", [
+        //auth,
+        check("user_name", "user_name is required").notEmpty(), // Check the user_name
+        check("visitor_name", "visitor_name is required").notEmpty(), // Check the visitor_name
+        check("visitor_phone", "Phone is required").notEmpty(), // Check the phone
+        check("visitor_phone", 
+        "Please enter phone number with 11 nums.").isLength(11), // Check the phone
+
+    ],
+    async(req, res) => {
+
+        try {
+            // Check for errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                // Return the errors
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const user_name = req.user_name;
+
+            // Use user_name select id
+            user_id = await promisePool.query(
+                `SELECT user_id from login WHERE user_name = "${user_name}"`
+            );
+
+            // Extract info from the body
+            let {
+                visitor_name,
+                visitor_phone,
+                visitor_gender,
+                visitor_avatar, // 头像url
+                emergency_name, // 紧急联系人
+                emergency_phone,// 紧急联系人电话
+            } = req.body;
+
+            // Create user object
+            let user = {
+                user_id,
+                user_name,
+                visitor_name,
+                visitor_gender,
+                visitor_phone,
+                visitor_avatar, // 头像url
+                emergency_name, // 紧急联系人
+                emergency_phone,// 紧急联系人电话                
+            };
+
+
+            
+            // Check gender
+            // gender == null
+            if(!visitor_gender){
+                visitor_gender = "Other";
+            } else if (
+                visitor_gender !== "Male" &&
+                visitor_gender !== "Female" &&
+                visitor_gender !== "Other"
+            ) {
+                return res.status(200).json({
+                    "data": {
+                        "errCode": 3,
+                        "errMessage": "性别非法（请填写Male/Female/Other）"
+                    }
+                });
+            }
+
+            try {
+
+                // Update details in students table
+                await promisePool.query(
+                    `UPDATE visitor SET visitor_name='${visitor_name}',
+                    visitor_phone='${visitor_phone}',
+                    visitor_gender='${visitor_gender}',
+                    visitor_avatar='${visitor_avatar}',
+                    emergency_name='${emergency_name}',
+                    emergency_phone='${emergency_phone}'
+                    WHERE visitor_id='${user_id}'`
+                );
+
+                return res.status(200).json({
+                    "data": {
+                        "errCode": 0,
+                        "errMessage": "成功"
+                    }
+                });
+
+
+                } catch (err) {
+                    // Catch errors
+                    throw err;
+                }
+            } catch (err) {
+            // Catch errors
+            throw err;
+        }
     }
 );
 
