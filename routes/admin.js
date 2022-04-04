3fd441ff43bf12c978cc07634d0ae75b8fd5947f
Northern-W <1547339416@qq.com>
2022-04-04 18:44:15 +0800

    /admin添加修改访客状态接口

--- routes/admin.js
+++ routes/admin.js
@@ -12,6 +12,7 @@ const router = express.Router();
  * 获取咨询师列表
  * 获取督导列表
  * 获取访客列表
+ * 修改访客状态
  */
 
 // @route   GET /admin/counsellorList
@@ -148,5 +149,58 @@ router.get("/visitorList", [
             throw err;
         }
     });
+
+// @route   POST /admin/changeVisitorStatus
+// @desc    修改访客状态
+// @access  Private
+router.post(
+    "/changeVisitorStatus", [
+    check("user_id", "user_id is required").notEmpty(), // Check the user_id
+  ],
+    async (req, res) => {
+      // Check for errorsy
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        // Return the errors
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      // Extract info from the body
+      let {
+        user_id,
+        status,
+      } = req.body;
+  
+        // Check status
+        if (status !== "normal" && status !== "banned"){
+                return res.status(401).json({ msg : "Error status."});
+        }
+
+      try {
+        // Check if user exists
+        const [rows] = await promisePool.query(
+          `SELECT EXISTS(SELECT * from login WHERE user_id = "${user_id}" ) "EXISTS" FROM dual`
+        );
+        const result = rows[0];
+  
+        if (!result) {
+          // User already exists
+          return res.status(400).json({ msg : "User doesn't exist."});
+        } 
+
+
+        await promisePool.query(
+            `UPDATE visitor SET visitor_status='${status}'
+                      WHERE visitor_id=${user_id}`
+          );
+  
+          return res.status(200).json({ msg :"status change success"});
+
+      } catch (err) {
+        // Catch errors
+        throw err;
+      }
+    }
+  );
 
 module.exports = router;
